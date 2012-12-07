27d57d2a9a093762168968492ea0524b01d57e0d
kathy <katthompson@aspenms.com>
2012-12-07 07:42:44 -0600

    styling page  text, button, repeating bg

--- gen/com/hello/world/R.java
+++ gen/com/hello/world/R.java
@@ -11,27 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int book=0x7f020000;
-        public static final int book_64=0x7f020001;
-        public static final int home=0x7f020002;
-        public static final int home_24=0x7f020003;
-        public static final int home_64=0x7f020004;
+        public static final int bg_grey_stripe=0x7f020000;
+        public static final int bg_repeat=0x7f020001;
+        public static final int blue_button=0x7f020002;
+        public static final int book=0x7f020003;
+        public static final int home=0x7f020004;
         public static final int ic_launcher=0x7f020005;
         public static final int man=0x7f020006;
-        public static final int man_24=0x7f020007;
-        public static final int man_64=0x7f020008;
-        public static final int phone=0x7f020009;
-        public static final int phone_24=0x7f02000a;
-        public static final int phone_64=0x7f02000b;
+        public static final int phone=0x7f020007;
     }
     public static final class id {
-        public static final int edit_message=0x7f070002;
         public static final int header_title=0x7f070001;
-        public static final int menu_calling=0x7f070005;
-        public static final int menu_contacts=0x7f070004;
-        public static final int menu_home=0x7f070003;
-        public static final int menu_profile=0x7f070006;
+        public static final int menu_calling=0x7f070008;
+        public static final int menu_contacts=0x7f070007;
+        public static final int menu_home=0x7f070006;
+        public static final int menu_profile=0x7f070009;
+        public static final int next_button=0x7f070005;
+        public static final int phone_number=0x7f070002;
+        public static final int pin_number=0x7f070004;
         public static final int textView1=0x7f070000;
+        public static final int title_pin=0x7f070003;
     }
     public static final class layout {
         public static final int activity_hello=0x7f030000;
@@ -52,6 +51,11 @@ public final class R {
         public static final int menu_contacts=0x7f040003;
         public static final int menu_home=0x7f040002;
         public static final int menu_profile=0x7f040005;
+        public static final int phone_number=0x7f040009;
+        public static final int pin_number=0x7f04000b;
+        public static final int recharge=0x7f04000c;
+        public static final int title_pin=0x7f04000a;
+        public static final int title_pin_summary=0x7f040008;
     }
     public static final class style {
         /** 
@@ -66,8 +70,8 @@ public final class R {
          */
         public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int customMenuTextColor=0x7f050002;
     }
 }
