6a63167f69bc538ad2878b3b424f7b0bb5696417
kathy <katthompson@aspenms.com>
2012-12-13 14:21:19 -0600

    map v2 not working in emulator

---
+++ src/com/hello/world/GoogleMapsActivity.java
@@ -0,0 +1,22 @@
+package com.hello.world;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GoogleMapsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_google_maps);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
